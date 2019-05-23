c887c0edef708faab0612be8f570339e11791c38
Ben Gosney <ben@evosite.co.uk>
2019-05-23 10:22:08 +0100

    Updates

---
+++ precache-manifest.2b4466a9d7990aa79c5eadaebb68ae84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51b630f2c5b85c23d8de",
+    "url": "/canvashills/static/js/runtime~main.8b48daa0.js"
+  },
+  {
+    "revision": "93f4794b928f3093d466",
+    "url": "/canvashills/static/js/main.825c5e02.chunk.js"
+  },
+  {
+    "revision": "64ac29518a272c8ac7f1",
+    "url": "/canvashills/static/js/2.a52a1d28.chunk.js"
+  },
+  {
+    "revision": "93f4794b928f3093d466",
+    "url": "/canvashills/static/css/main.961a655c.chunk.css"
+  },
+  {
+    "revision": "bf83ea5d8a9d17b502cfaeb7afa949b5",
+    "url": "/canvashills/index.html"
+  }
+];
